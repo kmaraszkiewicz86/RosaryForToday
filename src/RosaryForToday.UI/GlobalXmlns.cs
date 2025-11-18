cc10a571d681bd4470ec49de9edc7d5072ba3aa6
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2025-11-18 22:09:54 +0100

    Add initial empty UI project using .NET MAUI as app shell

---
+++ src/RosaryForToday.UI/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "RosaryForToday.UI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "RosaryForToday.UI.Pages")]
