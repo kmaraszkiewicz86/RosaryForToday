148d8528ce8865126ea6fc0610d26531e3edf97c
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2026-03-05 11:28:20 +0100

    Force respecting display cutouts

--- src/RosaryForToday.UI/Platforms/Android/MainActivity.cs
+++ src/RosaryForToday.UI/Platforms/Android/MainActivity.cs
@@ -1,11 +1,25 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace RosaryForToday.UI
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Force respecting display cutouts
+            if (OperatingSystem.IsAndroidVersionAtLeast(28))
+            {
+                if (Window?.Attributes is { } attributes)
+                {
+                    attributes.LayoutInDisplayCutoutMode = LayoutInDisplayCutoutMode.Default;
+                }
+            }
+        }
     }
 }
